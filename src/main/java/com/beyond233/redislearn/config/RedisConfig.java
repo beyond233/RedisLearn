2a4a36aad663ad2ce86ed8314b3c627b225fa2e2
XuJie <414460547@qq.com>
2020-08-13 19:30:10 +0800

    redis学习项目创建

---
+++ src/main/java/com/beyond233/redislearn/config/RedisConfig.java
@@ -0,0 +1,8 @@
+package com.beyond233.redislearn.config;
+
+/**
+ * @author XuJie
+ * @date 2020/8/13 19:29
+ */
+public class RedisConfig {
+}
---
+++ src/test/java/com/beyond233/redislearn/RedisOperatorTest.java
@@ -0,0 +1,14 @@
+package com.beyond233.redislearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisOperatorTest {
+
+    @Test
+    public void getValue() {
+
+    }
+
+}
