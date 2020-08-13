eed814261408f1b69965e5ae891cc64a027e7682
XuJie <414460547@qq.com>
2020-08-13 20:03:41 +0800

    redis配置

--- src/main/java/com/beyond233/redislearn/config/RedisConfig.java
+++ src/main/java/com/beyond233/redislearn/config/RedisConfig.java
@@ -1,8 +1,53 @@
 package com.beyond233.redislearn.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
 /**
+ * redis配置类
+ *
  * @author XuJie
  * @date 2020/8/13 19:29
  */
+@Configuration
+@EnableCaching
 public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        //1.设置连接工厂
+        template.setConnectionFactory(connectionFactory);
+
+        //2.使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        serializer.setObjectMapper(mapper);
+
+        template.setValueSerializer(serializer);
+
+        //3.使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(factory);
+        return stringRedisTemplate;
+    }
 }
--- src/test/java/com/beyond233/redislearn/RedisOperatorTest.java
+++ src/test/java/com/beyond233/redislearn/RedisOperatorTest.java
@@ -1,13 +1,24 @@
 package com.beyond233.redislearn;
 
+import com.beyond233.redislearn.util.RedisOperator;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class RedisOperatorTest {
 
+    @Autowired
+    private RedisOperator operator;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Test
     public void getValue() {
+        Object a = redisTemplate.opsForValue().get("a");
+        System.out.println(a);
 
     }
 
