856c90c1664637cc34bc4c5939e444ff3f0966be
XuJie <414460547@qq.com>
2020-08-14 17:49:31 +0800

    8.14

--- src/test/java/com/beyond233/redislearn/HashTest.java
+++ src/test/java/com/beyond233/redislearn/HashTest.java
@@ -1,59 +1,52 @@
 package com.beyond233.redislearn;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.beyond233.redislearn.pojo.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * <p>项目文档: </p>
- *
- * @author beyond233
- * @version 1.0
- * @since 2020-08-13 22:24
+ * Hash类型相关api测试
  */
 @SpringBootTest
 @Slf4j
-public class MapTest {
+public class HashTest {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
     @Test
-    public void read(){
+    public void read() {
         Object score = stringRedisTemplate.opsForHash().get("scoremap", "andy");
         Integer i = Integer.parseInt((String) score);
         System.out.println(i);
     }
 
     @Test
-    public void write(){
+    public void write() {
         User user = new User();
         user.setName("小徐");
         user.setGender("男");
         user.setAge(22);
 
         String userJson = JSON.toJSONString(user);
-        stringRedisTemplate.opsForHash().put("usermap","user:1",userJson);
+        stringRedisTemplate.opsForHash().put("usermap", "user:1", userJson);
     }
 
     /**
      * 读取并转换为对象
-     * */
+     */
     @Test
-    public void transObject(){
-        String userStr = (String)stringRedisTemplate.opsForHash().get("usermap", "user:1");
+    public void transObject() {
+        String userStr = (String) stringRedisTemplate.opsForHash().get("usermap", "user:1");
         System.err.println("读取到的字符串： " + userStr);
 
         User user = JSON.toJavaObject(JSON.parseObject(userStr), User.class);
 
         System.err.println("解析后的user对象： " + user.toString());
     }
+
+
 }
--- src/test/java/com/beyond233/redislearn/ValueTest.java
+++ src/test/java/com/beyond233/redislearn/ValueTest.java
@@ -8,8 +8,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
+/**
+ * String类型相关api测试
+ */
 @SpringBootTest
 @Slf4j
 class ValueTest {
@@ -23,12 +26,28 @@ class ValueTest {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    /**
+     * 自增、获取
+     */
     @Test
     public void getNumberValue() {
         Object a = redisTemplate.opsForValue().get("a");
-        System.out.println(a);
+        Long result = redisTemplate.opsForValue().increment("a", 10);
+        System.err.println(result);
+        System.err.println(redisTemplate.opsForValue().get("a"));
     }
 
-
+    /**
+     * 过期、获取剩余
+     */
+    @Test
+    public void expire() throws InterruptedException {
+        //添加值并设置过期时间
+        redisTemplate.opsForValue().set("a", "666", 60, TimeUnit.SECONDS);
+//        redisTemplate.expire("a", 60, TimeUnit.SECONDS);
+        Thread.sleep(4000);
+        System.err.println("key是否存活： " + redisTemplate.hasKey("a"));
+        System.err.println("key剩余存活时间：" + redisTemplate.getExpire("a", TimeUnit.SECONDS));
+    }
 
 }
